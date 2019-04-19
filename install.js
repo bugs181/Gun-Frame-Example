940c33dee48e69a2ab52979575a5ef6fd7abb777
Levi Roberts <bugs181@gmail.com>
2019-04-19 13:35:07 -0500

    Updated dependencies

--- install.js
+++ install.js
@@ -1,2 +1,2 @@
-// pnpm install --save-dev https://github.com/frame-js/Frame/tarball/master
-// pnpm install --save-dev https://github.com/amark/gun/tarball/master
+// pnpm install --save https://github.com/frame-js/Frame/tarball/master
+// pnpm install --save https://github.com/amark/gun/tarball/master
